f78c412b2dd0f200669dcbe6e68ffad706c509a9
Michał Sikora <me@mehow.io>
2026-07-31 17:27:00 +0200

    Handle restore on reinstall

--- sync/sync-impl/src/main/java/com/duckduckgo/sync/impl/ui/v2/SyncActivity.kt
+++ sync/sync-impl/src/main/java/com/duckduckgo/sync/impl/ui/v2/SyncActivity.kt
@@ -86,6 +86,7 @@ import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.ShowMessage
 import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.ShowPreviousSessionReady
 import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.ShowRecoveryCode
 import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.SyncWithAnotherDevice
+import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.OriginalFlow
 import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.SetupFlows.CreateAccountFlow
 import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.SetupFlows.SignInFlow
 import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.ViewState
@@ -219,6 +220,46 @@ class SyncActivity : DuckDuckGoActivity() {
             }
 
             is RecoverSyncedDataContract.Output.Dismissed -> Unit
+        }
+    }
+
+    private val previousSessionReadyLauncher = registerForActivityResult(
+        PreviousSessionReadyContract(),
+    ) { output ->
+        when (output) {
+            is PreviousSessionReadyContract.Output.Resume -> {
+                exchangeSyncCodeLauncher.launch(
+                    ExchangeSyncCodeContract.Input(
+                        syncUrl = output.recoveryCode,
+                        launchSource = launchSource,
+                        isRecoveryFlow = true,
+                    ),
+                )
+            }
+
+            is PreviousSessionReadyContract.Output.ContinueSetup -> {
+                viewModel.onContinueSetupAfterSkipRestore(output.originalFlow)
+            }
+
+            is PreviousSessionReadyContract.Output.Dismissed -> {
+                viewModel.onSyncThisDeviceCanceled()
+                viewModel.onConnectionCancelled()
+            }
+        }
+    }
+
+    private val exchangeSyncCodeLauncher = registerForActivityResult(
+        ExchangeSyncCodeContract(),
+    ) { output ->
+        when (output) {
+            is ExchangeSyncCodeContract.Output.SyncCompleted -> {
+                handlePairingResult(output.result, showConfirmationScreen = true)
+            }
+
+            is ExchangeSyncCodeContract.Output.Dismissed -> {
+                viewModel.onSyncThisDeviceCanceled()
+                viewModel.onConnectionCancelled()
+            }
         }
     }
 
@@ -416,7 +457,11 @@ class SyncActivity : DuckDuckGoActivity() {
             }
 
             is LaunchOriginalFlow -> {
-                logcat { "TODO: Handle ${command.javaClass.simpleName} command" }
+                when (command.originalFlow) {
+                    OriginalFlow.SYNC_THIS_DEVICE -> syncThisDeviceLauncher.launch(SyncThisDeviceContract.Input(launchSource))
+                    OriginalFlow.SYNC_WITH_ANOTHER -> syncWithAnotherDeviceLauncher.launch(ReadSyncCodeContract.Input(launchSource))
+                    OriginalFlow.RECOVER_SYNCED_DATA -> recoverSyncedDataLauncher.launch(RecoverSyncedDataContract.Input(launchSource))
+                }
             }
 
             is LaunchSyncGetOnOtherPlatforms -> {
@@ -447,7 +492,25 @@ class SyncActivity : DuckDuckGoActivity() {
             }
 
             is ShowPreviousSessionReady -> {
-                logcat { "TODO: Handle ${command.javaClass.simpleName} command" }
+                authenticate(
+                    onCancelled = {
+                        viewModel.onSyncThisDeviceCanceled()
+                        lifecycleScope.launch { syncSetupWideEvent.onUserAuthCancelled() }
+                    },
+                    onError = { message ->
+                        viewModel.onSyncThisDeviceCanceled()
+                        lifecycleScope.launch { syncSetupWideEvent.onUserAuthCancelled() }
+                        showError(ShowError(R.string.sync_simplified_error_dialog_generic_body, message))
+                    },
+                    onSuccess = { hasValidAuth ->
+                        if (hasValidAuth) {
+                            // authenticate() also passes if device is not enrolled into auth,
+                            // so only notify that auth was successful if it actually happened
+                            lifecycleScope.launch { syncSetupWideEvent.onUserAuthSuccess() }
+                        }
+                        previousSessionReadyLauncher.launch(PreviousSessionReadyContract.Input(command.originalFlow))
+                    },
+                )
             }
 
             // No-op in the simplified flow.
